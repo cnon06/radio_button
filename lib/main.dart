342d82e77ff351bb464f5ce7564c3290baec1a3c
Sinan Alagöz <sinanalagoz74@gmail.com>
2022-11-06 10:35:18 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:radio_button/main_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: MainPage()
+    );
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String? city = "London";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          RadioListTile<String>(
+              groupValue: city,
+              secondary: Icon(Icons.abc),
+              title: Text("London"),
+              value: "London",
+
+              //  groupValue: city,
+              onChanged: ((value) {
+                city = value;
+                setState(() {});
+              })),
+          RadioListTile<String>(
+              groupValue: city,
+              secondary: Icon(Icons.abc),
+              title: Text("Newyork"),
+              value: "Newyork",
+
+              //  groupValue: city,
+              onChanged: ((value) {
+                city = value;
+                setState(() {});
+              })),
+          RadioListTile<String>(
+              groupValue: city,
+              secondary: Icon(Icons.abc),
+              title: Text("Tokyo"),
+              value: "Tokyo",
+
+              //  groupValue: city,
+              onChanged: ((value) {
+                city = value;
+                setState(() {});
+              })),
+        ],
+      )),
+    );
+  }
+}
